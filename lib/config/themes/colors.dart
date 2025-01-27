4b27dc62fc671740e5fb2979f45993a2a7f03e8e
Muhammad <oayodele@afexnigeria.com>
2025-01-27 16:06:56 +0100

    Folder structure

---
+++ lib/config/themes/colors.dart
@@ -0,0 +1,23 @@
+class AppColor{
+  static const Color primaryBlue = Color.fromRGBO(144, 205, 255, 1);
+  static const Color backgroundWhite = Color.fromRGBO(255, 255, 255, 1);
+  static const Color lightBlue = Color.fromRGBO(231, 244, 255, 1);
+  static const Color blueBase = Color.fromRGBO(78, 175, 255, 1);
+  static const Color secondaryLightBlue = Color.fromRGBO(196, 228, 255, 1);
+  static const Color greylight = Color.fromRGBO(25, 28, 28, 0.70);
+  static const Color primary = Color(0xFF25435B);
+  static const Color brownBlack = Color.fromRGBO(51, 51, 51, 1);
+  static const Color lightBlack = Color.fromRGBO(153, 153, 153, 1);
+  static const Color lightGrey = Color.fromRGBO(227, 229, 229, 1);
+  static const Color textLight = Color(0xff303437);
+  static const Color skyLightest = Color(0xFFF7F9FA);
+  static const Color inkDarkest = Color(0xff090A0A);
+  static const Color inkDarker = Color(0xff202325);
+  static const Color inkLighter = Color(0xff72777A);
+  static const Color inkLight = Color(0xffE3E5E5);
+  static const Color primaryBase = Color(0xff4EAFFF);
+  static const Color primaryLightest = Color(0xffE7F4FF);
+  static const Color skyLighter = Color(0xffF2F4F5);
+  static const Color inkBase = Color(0xff404446);
+  static const Color skyDark = Color(0xff979C9E);
+}
